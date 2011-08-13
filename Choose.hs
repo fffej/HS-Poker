ce69879e9e26a389a83ed32bff2dfe7a4c4eabdb
Jeff Foster <jeff.foster@acm.org>
2011-08-13 15:54:54 +0100

    steal some code for choose n k

---
+++ Choose.hs
@@ -0,0 +1,7 @@
+module Choose where
+
+combinationsOf 0 _ = [[]]
+combinationsOf _ [] = []
+combinationsOf k (x:xs) = map (x:) (combinationsOf (k-1) xs) ++ combinationsOf k xs
+
+combinations k n = combinationsOf k [1..n]
