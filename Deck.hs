02404e3f099d828bc180c3c7b6cb06cc740a8798
Jeff Foster <jeff.foster@acm.org>
2011-08-13 11:30:13 +0100

    initial commit

---
+++ Deck.hs
@@ -0,0 +1,82 @@
+module Deck where
+
+import Data.List
+import Data.Maybe
+
+data Suit = Hearts | Diamonds | Spades | Clubs deriving (Show,Eq)
+
+data Value = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace
+           deriving (Show,Enum,Eq,Ord)
+
+cc :: Char -> Int -> Card
+cc c 1 = Card (mkSuit c) Ace
+cc c n = Card (mkSuit c) (mkValue n)
+
+mkSuit :: Char -> Suit
+mkSuit 'D' = Diamonds
+mkSuit 'C' = Clubs
+mkSuit 'S' = Spades
+mkSuit 'H' = Hearts
+mkSuit _ = error "Are you mad?  What type of card is that!"
+
+mkValue :: Int -> Value
+mkValue 1 = Ace
+mkValue n = toEnum (n - 2)
+
+data Card = Card Suit Value
+
+getSuit :: Card -> Suit
+getSuit (Card x _) = x
+
+getValue :: Card -> Value
+getValue (Card _ v) = v
+
+instance Show Card where
+  show (Card suit value) = show value ++ " of " ++ show suit
+
+data Deck = Deck [Card] deriving Show
+
+data Hand = Hand [Card]
+
+data BestHand = StraightFlush Value -- highest card
+              | FourOfAKind Value Value -- four of a kind, plus kicker
+              | FullHouse Value Value -- 3 and 2
+              | Flush Value -- highest card
+              | Straight Value -- highest card
+              | ThreeOfAKind Value Value Value -- three of a kind, plus kickers
+              | TwoPairs Value Value Value -- two pairs, plus kicker
+              | OnePair Value Value Value Value -- one pair, 3 kickers
+              | HighCard Value Value Value Value Value
+                deriving (Show,Eq)
+  
+getBestHand :: Card -> Card -> Card -> Card -> Card -> BestHand
+getBestHand a b c d e | isJust straightFlush' = fromJust straightFlush' 
+  where
+    straightFlush' = straightFlush a b c d e
+                                                             
+allSameSuit :: [Card] -> Bool
+allSameSuit [] = True
+allSameSuit (x:xs) = all (\c -> getSuit x == getSuit c) xs
+  
+contiguousValues :: [Card] -> Bool
+contiguousValues xs | length uniqValues < 5 = False
+                    | (highest - lowest) == 4 = True
+                    | highest == 12 && aceCondition = True
+                    | otherwise = False
+  where
+    uniqValues = nub $ sort $ map getValue xs 
+    lowest = fromEnum $ head uniqValues
+    highest = fromEnum $ last uniqValues
+    aceCondition = lowest == 2 && (fromEnum $ last (init uniqValues)) == 5
+
+maxValue :: [Card] -> Value
+maxValue = maximum . (map getValue)
+
+straightFlush :: Card -> Card -> Card -> Card -> Card -> Maybe BestHand
+straightFlush a b c d e | allSameSuit cards && contiguousValues cards = Just $ StraightFlush (maxValue cards)
+                        | otherwise = Nothing
+  where
+    cards = [a,b,c,d,e]
+
+createOrderedDeck :: Deck
+createOrderedDeck = Deck [Card suit value | suit <- [Hearts,Diamonds,Spades,Clubs], value <- enumFromTo Two Ace]
---
+++ DeckTest.hs
@@ -0,0 +1,24 @@
+module DeckTest where
+
+import Deck
+import Test.HUnit
+
+h = 'H'
+d = 'D'
+s = 'S'
+c = 'C'
+
+getBestHandFromList :: [Card] -> BestHand
+getBestHandFromList (a:b:c:d:e:[]) = getBestHand a b c d e 
+
+straightFlushFromList :: [Card] -> Maybe BestHand
+straightFlushFromList (a:b:c:d:e:[]) = straightFlush a b c d e 
+
+testStraightFlushPositive = TestCase $ assertEqual
+               "StraightFlush with value" (Just $ StraightFlush Six) (straightFlushFromList [cc h s| s <- [2,3,4,5,6]]) 
+
+testStraightFlushNegative = TestCase $ assertEqual
+               "Not a StraightFlush with value" Nothing (straightFlushFromList [cc h s| s <- [2,3,4,7,6]]) 
+
+
+main = runTestTT $ TestList [testStraightFlushPositive,testStraightFlushNegative]
