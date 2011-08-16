69c15045cac8521aac57cab1763c2dab0926084f
Jeff Foster <jeff.foster@acm.org>
2011-08-16 20:45:27 +0100

    get the constituents for turning it into a card

--- cactus-kev/Cactus.hs
+++ cactus-kev/Cactus.hs
@@ -5,23 +5,54 @@ import Data.Bits
 
 data Rank = Two | Three | Four | Five | Six | Seven
           | Eight | Nine | Ten | Jack | Queen 
-          | King | Ace
+          | King | Ace deriving (Enum,Ord,Eq,Show)
                    
-data Suit = Heart | Diamond | Club | Spade
+data Suit = Heart | Diamond | Club | Spade deriving (Enum,Ord,Eq,Show)
 
-newtype Card = Card Word64
+newtype Card = Card Word32
                    
-assignedPrime :: Rank -> Int
-assignedPrime Two = 2
-assignedPrime Three = 3
-assignedPrime Four = 5
-assignedPrime Five = 7 
-assignedPrime Six = 11
-assignedPrime Seven = 13
-assignedPrime Eight = 17
-assignedPrime Nine = 19
-assignedPrime Ten = 23
-assignedPrime Jack = 29
-assignedPrime Queen = 31
-assignedPrime King = 37
-assignedPrime Ace = 41
+               
+{-
+   +--------+--------+--------+--------+
+   |xxxbbbbb|bbbbbbbb|cdhsrrrr|xxpppppp|
+   +--------+--------+--------+--------+
+
+   p = prime number of rank (deuce=2,trey=3,four=5,...,ace=41)
+   r = rank of card (deuce=0,trey=1,four=2,five=3,...,ace=12)
+   cdhs = suit of card (bit turned on based on suit of card)
+   b = bit turned on depending on rank of card
+-}
+mkCard :: Rank -> Suit -> Card
+mkCard r s = undefined
+  where
+    cdhs = suit s
+    rrrr = cardRank r
+    p = primeRank r
+    b = cardBit r
+               
+suit :: Suit -> Word8        
+suit Club = bit 7
+suit Diamond = bit 6
+suit Heart = bit 5
+suit Spade = bit 4
+        
+cardBit :: Rank -> Word16
+cardBit = bit . fromIntegral . fromEnum
+              
+cardRank :: Rank -> Word8
+cardRank = fromIntegral . fromEnum 
+                   
+primeRank :: Rank -> Word8
+primeRank Two = 2
+primeRank Three = 3
+primeRank Four = 5
+primeRank Five = 7 
+primeRank Six = 11
+primeRank Seven = 13
+primeRank Eight = 17
+primeRank Nine = 19
+primeRank Ten = 23
+primeRank Jack = 29
+primeRank Queen = 31
+primeRank King = 37
+primeRank Ace = 41
