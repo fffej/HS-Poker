a6c32f8665998af3333fefc4760647b7f7f2f402
Jeff Foster <jeff.foster@acm.org>
2011-08-16 20:54:04 +0100

    make the Word32

--- cactus-kev/Cactus.hs
+++ cactus-kev/Cactus.hs
@@ -23,26 +23,31 @@ newtype Card = Card Word32
    b = bit turned on depending on rank of card
 -}
 mkCard :: Rank -> Suit -> Card
-mkCard r s = undefined
+mkCard r s = Card (mkCardWord64 r s)
+               
+mkCardWord64 :: Rank -> Suit -> Word32
+mkCardWord64 r s =  (p .|. byte2 .|. byte34) 
   where
     cdhs = suit s
     rrrr = cardRank r
+    byte2 = cdhs .|. rrrr
     p = primeRank r
     b = cardBit r
-               
-suit :: Suit -> Word8        
+    byte34 = shiftL b 16
+             
+suit :: Suit -> Word32
 suit Club = bit 7
 suit Diamond = bit 6
 suit Heart = bit 5
 suit Spade = bit 4
         
-cardBit :: Rank -> Word16
+cardBit :: Rank -> Word32
 cardBit = bit . fromIntegral . fromEnum
               
-cardRank :: Rank -> Word8
+cardRank :: Rank -> Word32
 cardRank = fromIntegral . fromEnum 
                    
-primeRank :: Rank -> Word8
+primeRank :: Rank -> Word32
 primeRank Two = 2
 primeRank Three = 3
 primeRank Four = 5
